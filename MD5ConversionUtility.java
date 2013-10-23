43629dbf96e8c7a53c3dbe98a5fa2b360a5d2a89
ankurgoelhc <ankurgoel.iiit@gmail.com>
2013-10-23 15:37:07 +0530

    Create MD5ConversionUtility.java

---
+++ MD5ConversionUtility.java
@@ -0,0 +1,28 @@
+package com.test.ankur;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5ConversionUtility 
+{
+    public static void main(String[] args)throws Exception {
+    	System.out.println(encryptDataInHEXFormat("baavgavha"));
+    	
+    }
+    
+    public static String encryptDataInHEXFormat(String data) throws NoSuchAlgorithmException{
+    	 MessageDigest md = MessageDigest.getInstance("MD5");
+         md.update(data.getBytes());
+  
+         byte byteData[] = md.digest();
+  
+         //convert the byte to hex format method 1
+         StringBuffer sb = new StringBuffer();
+         for (int i = 0; i < byteData.length; i++) {
+          sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+         }
+         
+         return sb.toString();
+    }
+    
+}
